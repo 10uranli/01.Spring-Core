c5051fc0273c6112131073aab317ee437dd80d5d
onur anli <onur.anli@bil.omu.edu.tr>
2023-06-21 19:12:04 +0300

    component scan
    component scan

---
+++ spring-core/src/main/java/com/spring/springcore/_12/componentScan/Insurance.java
@@ -0,0 +1,13 @@
+package com.spring.springcore._12.componentScan;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Insurance {
+
+    private String type;
+}
---
+++ spring-core/src/main/java/com/spring/springcore/_12/componentScan/_12ExecuteClass.java
@@ -0,0 +1,26 @@
+package com.spring.springcore._12.componentScan;
+
+
+import com.spring.springcore._11.javaBasedConfig.Car;
+import com.spring.springcore._11.javaBasedConfig.Config;
+import com.spring.springcore._11.javaBasedConfig.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class _12ExecuteClass {
+
+
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("_12.componentScan.xml");
+
+		Insurance insurance = ctx.getBean(Insurance.class);
+		insurance.setType("Dask");
+
+		System.out.println(insurance);
+	}
+
+}
